f819511cb376f1e827b0e82ce0e4dbf5d6a31f82
Thomas Cannon <tom-cannon@playcannon.com>
2021-10-20 11:46:51 -0700

    Added p5

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import * as P5 from 'p5'
+
+/* eslint-disable-next-line no-new */
+new P5(s => {
+  s.setup = () => {
+    s.createCanvas(100, 100)
+  }
+
+  s.draw = () => {
+    /* TODO */
+  }
+})
